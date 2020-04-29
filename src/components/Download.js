82f467aa0fe62e14169437fe145fb35c25467199
matthieulegouis <matthieu.legouis@gmail.com>
2020-04-30 00:40:31 +1000

    Update Download.js

--- src/components/Download.js
+++ src/components/Download.js
@@ -27,6 +27,28 @@ const Img = styled.img`
 
 export default (props) => {
   const [statusImage, setStatusImage] = useState("ready");
+  function fixBinary (bin) {
+    var length = bin.length;
+    var buf = new ArrayBuffer(length);
+    var arr = new Uint8Array(buf);
+    for (var i = 0; i < length; i++) {
+      arr[i] = bin.charCodeAt(i);
+    }
+    return buf;
+  }
+  var base64 = 
+    "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAB1klEQVR42n2TzytEURTHv3e8N1joRhZG" + 
+    "zJsoCjsLhcw0jClKWbHwY2GnLGUlIfIP2IjyY2djZTHSMJNQSilFNkz24z0/Ms2MrnvfvMu8mcfZvPvu" + 
+    "Pfdzz/mecwgKLNYKb0cFEgXbRvwV2s2HuWazCbzKA5LvNecDXayBjv9NL7tEpSNgbYzQ5kZmAlSXgsGG" + 
+    "XmS+MjhKxDHgC+quyaPKQtoPYMQPOh5U9H6tBxF+Icy/aolqAqLP5wjWd5r/Ip3YXVILrF4ZRYAxDhCO" + 
+    "J/yCwiMI+/xgjOEzmzIhAio04GeGayIXjQ0wGoAuQ5cmIjh8jNo0GF78QwNhpyvV1O9tdxSSR6PLl51F" + 
+    "nIK3uQ4JJQME4sCxCIRxQbMwPNSjqaobsfskm9l4Ky6jvCzWEnDKU1ayQPe5BbN64vYJ2vwO7CIeLIi3" + 
+    "ciYAoby0M4oNYBrXgdgAbC/MhGCRhyhCZwrcEz1Ib3KKO7f+2I4iFvoVmIxHigGiZHhPIb0bL1bQApFS" + 
+    "9U/AC0ulSXrrhMotka/lQy0Ic08FDeIiAmDvA2HX01W05TopS2j2/H4T6FBVbj4YgV5+AecyLk+Ctvms" + 
+    "QWK8WZZ+Hdf7QGu7fobMuZHyq1DoJLvUqQrfM966EU/qYGwAAAAASUVORK5CYII=";
+
+  var binary = fixBinary(atob(base64));
+  var blobDummy = new Blob([binary], {type: 'image/png'});
 
   // Save previewed poster in PNG
   const savePng = async () => {
@@ -38,9 +60,8 @@ export default (props) => {
     const canvas = await getCanvas();
     canvas.toBlob(blob => {
       try {
-        setTimeout(saveAs(blob, "avatar.png"), 0);
-        //saveAs(blob, "avatar.png");
-        //setStatusImage("ready");
+        saveAs(blobDummy, "avatar.png");
+        setStatusImage("ready");
       } catch (e) {
         console.error(e);
         setStatusImage("error");
