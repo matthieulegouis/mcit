68e676d595e59f1b9223ebc289aec480e340831b
matthieulegouis <matthieu.legouis@gmail.com>
2020-04-30 16:32:30 +1000

    Update Download.js

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ButtonMui from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import { saveAs } from "file-saver";
-import html2canvas from "html2canvas";
-import "canvas-toBlob";
-
+import getCanvas from "../helpers/getCanvas";
 
 const Button = styled(ButtonMui).attrs({
   style: {
@@ -29,40 +27,25 @@ const Img = styled.img`
 
 export default (props) => {
   const [statusImage, setStatusImage] = useState("ready");
-  const [canvas, setCanvas] = useState();
-
-  useEffect(() => {
-    var img;
-    const lastScroll = window.scrollY;
-    window.scrollTo(0, 0);
-    const ref = document.querySelector("#screensaver");
-    ref.style.opacity = 1;
-
-    html2canvas(ref).then(function(canvas) {
-      
-      setCanvas(canvas);
-  });
-    console.log("a", img)
-    ref.style.opacity = 0;
-    window.scroll(0, lastScroll);
-  }, [props]);
 
   // Save previewed poster in PNG
   const savePng = () => {
-
-    const a = document.createElement("a");
-    document.body.appendChild(a);
-    a.href = canvas.toDataURL("image/png", 0.1);
-    a.download = "canvas.png";
-    a.click();
+    var canvas = document.getElementById("myCanvas");
 
 
+    
 
+
+    canvas.toBlob((blob) => {
+      saveAs(blob, "pretty image.png");
+    });
 
   };
 
   return (
     <Grid container direction="column" justify="center" alignItems="center">
+      <img id="scream" src="https://cdn.dribbble.com/users/24711/screenshots/11192482/media/18922ef20f8af11ad104d2d853d61059.jpg" alt="The Scream" width="220" height="277" />
+      <canvas id="myCanvas" width="250" height="300"></canvas>
       {props.facebook ?
         <Button variant="contained" color="primary" onClick={savePng}>
           <Img src="/images/social/facebook.svg" />
